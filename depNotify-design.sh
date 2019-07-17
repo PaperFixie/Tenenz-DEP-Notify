6fd498575dd3188e1f4562023cf54e2a81618639
Vince Mascoli <v@vincemascoli.com>
2019-07-16 19:51:50 -0500

    updated DEPNotify scripts

--- depNotify-design.sh
+++ depNotify-design.sh
@@ -76,6 +76,7 @@
 # The policy array must be formatted "Progress Bar text,customTrigger". These will be
 # run in order as they appear below.
   POLICY_ARRAY=(
+    "Setting Timezone...,set_timezone"
     "Naming Mac...,name_mac"
     "Installing Utilities...,dockutil"
     "Installing Utilities...,fonts"
@@ -86,7 +87,7 @@
     "Installing FileMaker Plug-Ins...,fmp15_plugins"
     "Installing FileMaker Pro 16...,filemaker16"
     "Installing FileMaker Plug-Ins...,fmp16_plugins"
-    "Installing Microsoft Office 2011...,office2011"
+    "Installing Microsoft Office...,office365"
 #    "Installing Adobe Creative Cloud...,adobe_cc" ## currently not possible with our account
     "Installing Chrome...,chrome"
     "Installing Firefox...,firefox"
--- depNotify-dev.sh
+++ depNotify-dev.sh
@@ -76,6 +76,7 @@
 # The policy array must be formatted "Progress Bar text,customTrigger". These will be
 # run in order as they appear below.
   POLICY_ARRAY=(
+    "Setting Timezone...,set_timezone"
     "Naming Mac...,name_mac"
     "Installing Utilities...,dockutil"
     "Installing Utilities...,fonts"
@@ -86,12 +87,11 @@
     "Installing FileMaker Plug-Ins...,fmp15_plugins"
     "Installing FileMaker Pro 16...,filemaker16"
     "Installing FileMaker Plug-Ins...,fmp16_plugins"
-    "Installing Microsoft Office 2011...,office2011"
+    "Installing Microsoft Office...,office365"
     "Installing Chrome...,chrome"
     "Installing Firefox...,firefox"
     "Installing Slack...,slack"
     "Installing Atom...,atom"
-    "Finding Shortcuts...,server_shortcuts"
     "Adding Printers,install_printers"
     "Swabbing Deck...,set_dock_items_dev"
     "Polishing Apple...,set_tenenz_wallpaper"
